205abae35e281be3c57781390ac917997e7624b1
Christopher N. Hesse <raymanfx@gmail.com>
2015-12-27 15:20:41 +0100

    staging: samsung: sec_argos: Fix irq_set_affinity usage
    Change-Id: I875cc80c53c249421efc55113321e7b6566b861e

--- drivers/staging/samsung/sec_argos.c
+++ drivers/staging/samsung/sec_argos.c
@@ -25,6 +25,7 @@
 #include <linux/list.h>
 #include <linux/cpumask.h>
 #include <linux/sec_argos.h>
+#include <linux/interrupt.h>
 
 #define ARGOS_NAME "argos"
 #define TYPE_SHIFT 4
--- include/linux/sec_argos.h
+++ include/linux/sec_argos.h
@@ -1,7 +1,6 @@
 #ifndef _SEC_ARGOS_H
 #define _SEC_ARGOS_H
 
-extern int irq_set_affinity(unsigned int irq, const struct cpumask *mask);
 extern void exynos_dm_hotplug_enable(void);
 extern void exynos_dm_hotplug_disable(void);
 
